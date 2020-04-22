c62da7a640cd6b84a5d19feb886085e267390e8e
eddcas01@gmail.com <eddcas01@gmail.com>
2020-04-22 08:59:06 -0600

    Primer Pull

---
+++ Lab2Vista/Lab2Controlador/conexion.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Odbc;
+
+namespace Lab2Controlador
+{
+    public class conexion
+    {
+        public OdbcConnection Conexion()
+        {
+            //creacion de la conexion via ODBC
+            OdbcConnection conn = new OdbcConnection("Dsn=LB");
+            try
+            {
+                conn.Open();
+            }
+            catch (OdbcException)
+            {
+                Console.WriteLine("No Conectó");
+            }
+            return conn;
+        }
+
+        //metodo para cerrar la conexion
+        public void desconexion(OdbcConnection conn)
+        {
+            try
+            {
+                conn.Close();
+            }
+            catch (OdbcException)
+            {
+                Console.WriteLine("No Conectó");
+            }
+        }
+
+    
+}
+}
---
+++ Lab2Vista/Lab2Modelo/modelo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Lab2Controlador;
+using System.Data.Odbc;
+
+namespace Lab2Modelo
+{
+    class modelo
+    {
+        sentencias  sn = new sentencias();
+    }
+}
