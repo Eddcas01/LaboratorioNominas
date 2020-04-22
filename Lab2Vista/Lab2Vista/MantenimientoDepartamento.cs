2e9769942504fa20754863da7ee7802a5f3950ea
eddcas01@gmail.com <eddcas01@gmail.com>
2020-04-22 10:18:10 -0600

    MDI

---
+++ Lab2Vista/Lab2Vista/MantenimientoDepartamento.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab2Vista
+{
+    public partial class MantenimientoDepartamento : Form
+    {
+        string usuario = "";
+        public MantenimientoDepartamento(string user)
+        {
+            InitializeComponent();
+
+            usuario = user;
+            string[] alias = { "cod_depto","Nombre depto","estado" };
+            navegador1.asignarAlias(alias);
+            navegador1.asignarSalida(this);
+            Color Hotel = Color.FromArgb(253, 254, 254);
+            navegador1.asignarColorFondo(Hotel);
+            navegador1.asignarColorFuente(Color.Black);
+            navegador1.asignarAyuda("1");
+          
+            navegador1.asignarTabla("departamento");
+            navegador1.asignarNombreForm("");
+        }
+
+        private void Navegador1_Load(object sender, EventArgs e)
+        {
+            string aplicacionActiva = "1";
+            navegador1.ObtenerIdUsuario(usuario);
+            navegador1.botonesYPermisosInicial(usuario, aplicacionActiva);
+            navegador1.ObtenerIdAplicacion(aplicacionActiva);
+        }
+    }
+}
