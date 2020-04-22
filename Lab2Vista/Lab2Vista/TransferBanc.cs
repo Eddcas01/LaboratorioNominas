97bc08ede4f60eefefbab99a3efd534fef3fac05
eddcas01@gmail.com <eddcas01@gmail.com>
2020-04-22 12:22:41 -0600

    Mantenimientos y Procesos

---
+++ Lab2Vista/Lab2Vista/TransferBanc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab2Vista
+{
+    public partial class TransferBanc : Form
+    {
+        public TransferBanc()
+        {
+            InitializeComponent();
+        }
+    }
+}
